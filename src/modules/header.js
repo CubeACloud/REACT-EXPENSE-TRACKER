e1420f0a7f560e7a5b5a14356b5963e1e3ea9382
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-06-20 15:55:13 +0500

    REACT EXPENSE TRACKER
    react expense tracker by mudassir khan

---
+++ src/modules/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+    <div>
+        <h3>
+            Expense Tracker By MUDASSIR KHAN
+        </h3>
+    </div>
+    )
+}
